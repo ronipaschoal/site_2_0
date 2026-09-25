2bb6a254775f980fff6e7bdda1399ca6f821f728
Roni Paschoal <ronipaschoal@gmail.com>
2026-09-25 11:44:19 -0300

    refactor: raise small-screen breakpoint to 900px

--- lib/core/media_query_helper.dart
+++ lib/core/media_query_helper.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/widgets.dart';
 
 extension RpResponsive on BuildContext {
-  static const _smallScreenBreakpoint = 600.0;
+  static const _smallScreenBreakpoint = 900.0;
 
   bool get isSmallScreen =>
       MediaQuery.sizeOf(this).width < _smallScreenBreakpoint;
